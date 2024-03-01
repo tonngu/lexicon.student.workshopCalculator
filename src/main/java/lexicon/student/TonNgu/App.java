a8252deb70bb9a90fb41c1520a8b7f43a39f6f1e
Tony Nguyen <tonynguyen7396@gmail.com>
2024-03-01 16:10:13 +0100

    Added choice to terminate application

--- src/main/java/lexicon/student/TonNgu/App.java
+++ src/main/java/lexicon/student/TonNgu/App.java
@@ -1,25 +1,31 @@
 package lexicon.student.TonNgu;
 
-import com.sun.org.apache.xerces.internal.impl.XMLEntityScanner;
+import java.util.Scanner;
 
-import java.util.Scanner
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in)
-        System.out.println("Console Calculator App");
-        System.out.println("Input the first number:");
-        double n1 = scanner.nextDouble();
-        System.out.println("Input the second number:");
-        double n2 = scanner.nextDouble();
-        System.out.println("Choose operation type, enter number:");
-        System.out.println("1. Addition");
-        System.out.println("2. Subtraction");
-        System.out.println("3. Multiplication");
-        System.out.println("4. Division");
+        Scanner scanner = new Scanner(System.in);
+        boolean goAgain = true
+        while (goAgain = true) {
+            System.out.println("Console Calculator App");
+            System.out.println("Choose one of the following operation type, enter number:");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Terminate Application");
+            int operationChoice = scanner.nextInt();
+            if (operationChoice = 5) {
+            goAgain = false};
+            System.out.println("Input the first number:");
+            double n1 = scanner.nextDouble();
+            System.out.println("Input the second number:");
+            double n2 = scanner.nextDouble();
 
+                
+            }
+        System.out.println("Application Terminated.");
+        }
     }
 }
